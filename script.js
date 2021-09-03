4360cf8cccdab158e6f23daa4797ac9add7e5b3b
mrhxszo <dhungananischal76@gmail.com>
2021-09-04 00:11:07 +0200

    added GreyScale mode and butoon pressed style

--- script.js
+++ script.js
@@ -6,34 +6,122 @@ const slider = document.querySelector(".slider");
 const color = document.querySelector(".color");
 const rainbow = document.querySelector(".rainbow");
 const pixels = document.querySelector(".pixels")
+const greyscale = document.querySelector(".greyscale");
 height = "500px";
 width = "500px";
 container.style.height = height;
 container.style.width = width;
 let newBox;
 let newBoxes;
+let box = [];
 
 defaultResolution();
 slider.addEventListener("input", resolution);
 
-clear.onclick = function () {
+clear.onclick = function (e) {
     newBoxes.forEach((element) => element.style.backgroundColor = "#f7f3d6");
+    for(let i=0; i<box.length; i++){
+        box[i] = 0;
+    }
 }
 
 eraser.onclick = function () {
     newBoxes.forEach((newBox) => newBox.addEventListener("mouseover", () => newBox.style.backgroundColor = "#f7f3d6"));
+    let currentMode = "eraser";
+    buttonColor(currentMode);
 }
 
 pen.onclick = function () {
     newBoxes.forEach((newBox) => newBox.addEventListener("mouseover", () => newBox.style.backgroundColor = color.value));
+    let currentMode = "pen";
+    buttonColor(currentMode);
 }
 
-rainbow.onclick = function () {
-    newBoxes.forEach((newBox) => newBox.addEventListener("mouseover", () => {
+rainbow.onclick = function (e) {
+    console.log(e);
+    newBoxes.forEach((newBox) => newBox.addEventListener("mouseover", function rainbowfunc(){
         let red = Math.random()*100;
         let green = Math.random()*100;
         let blue = Math.random()*100;
-        newBox.style.backgroundColor = `rgb(${red}%,${green}%,${blue}%)`}));
+        newBox.style.backgroundColor = `rgb(${red}%,${green}%,${blue}%)`
+    }));
+    let currentMode = "rainbow";
+    buttonColor(currentMode);
+}
+
+greyscale.onclick = function () {
+    newBoxes.forEach((i) => i.removeEventListener("mouseover", function rainbowfunc(){
+        let red = Math.random()*100;
+        let green = Math.random()*100;
+        let blue = Math.random()*100;
+        i.style.backgroundColor = `rgb(${red}%,${green}%,${blue}%)`
+    }));
+    let arr = [];
+    let a = 0;
+    let b= 0;
+    newBoxes.forEach(() => box.push(0.1));
+    newBoxes.forEach(() => arr.push(a +=1 ));
+    newBoxes.forEach((i) => {
+        greyScale(i, arr.indexOf(b += 1));
+    });
+    let currentMode = "greyscale";
+    buttonColor(currentMode);
+}
+
+
+
+function greyScale(i, a){
+    i.onmouseover = function () {
+        //   if(box[a] >= 1){
+        //     return;
+        // }
+        let newcolor = hexToRgb(`${color.value}`);
+        newBoxes[a].style.backgroundColor = `rgba(${newcolor[0]},${newcolor[1]},${newcolor[2]}, ${box[a]})`;
+        box[a] +=0.15;
+    }
+}
+
+function buttonColor(currentMode){
+    console.log(currentMode);
+    switch (currentMode){
+        case 'eraser':
+            if (eraser.classList.contains("buttonpressed")== true){
+                return;
+            }
+            eraser.classList.add("buttonpressed");
+            pen.classList.remove("buttonpressed");
+            rainbow.classList.remove("buttonpressed");
+            greyscale.classList.remove("buttonpressed");
+            break;
+        case'pen':
+            if (pen.classList.contains("buttonpressed")== true){
+                return;
+            }
+            eraser.classList.remove("buttonpressed");
+            pen.classList.add("buttonpressed");
+            rainbow.classList.remove("buttonpressed");
+            greyscale.classList.remove("buttonpressed");
+            break;
+        case("rainbow"):
+            if (rainbow.classList.contains("buttonpressed")== true){
+                return;
+            }
+            eraser.classList.remove("buttonpressed");
+            pen.classList.remove("buttonpressed");
+            rainbow.classList.add("buttonpressed");
+            greyscale.classList.remove("buttonpressed");
+            break;
+        case("greyscale"):
+            if (greyscale.classList.contains("buttonpressed")== true){
+                return;
+            }
+            eraser.classList.remove("buttonpressed");
+            pen.classList.remove("buttonpressed");
+            rainbow.classList.remove("buttonpressed");
+            greyscale.classList.add("buttonpressed");
+            break;
+
+    }
 }
 
 function EventListenerAdder(){
@@ -49,8 +137,19 @@ function defaultResolution(){
         container.appendChild(newBox);
     }
     EventListenerAdder();
-    pixels.textContent = `${slider.value} x ${slider.value} px`;
+    pixels.textContent = `${slider.value} x ${slider.value} boxes`;
 }
+
+function hexToRgb(hex) {
+    hex = hex.replace(/[^0-9A-F]/gi, '');
+    var bigint = parseInt(hex, 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+
+    return [(r),(g),(b)];
+}
+  
 
 function resolution(e){
     clearpad();
